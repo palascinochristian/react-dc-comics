0435e1ccde373e382f4ed0e500c1ec689880f613
palascinochristian <palascinochristian@gmail.com>
2025-01-28 17:53:43 +0100

    feat: add page header

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,3 +1,41 @@
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <nav className="container">
+        <img className="img-fluid" src="/imgs/dc-logo.png" />
+        <ul>
+          <li>
+            <a href="#">Characters</a>
+          </li>
+          <li>
+            <a href="#">Comics</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">Tv</a>
+          </li>
+          <li>
+            <a href="#">Games</a>
+          </li>
+          <li>
+            <a href="#">Collectibles</a>
+          </li>
+          <li>
+            <a href="#">Videos</a>
+          </li>
+          <li>
+            <a href="#">Fans</a>
+          </li>
+          <li>
+            <a href="#">News</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
