ab7a01cdce1d075de3bba8b60d1e77f33d8d4ed8
palascinochristian <palascinochristian@gmail.com>
2025-01-28 19:45:52 +0100

    feat: add icons section

--- react-dc-comics/src/components/Icons.jsx
+++ react-dc-comics/src/components/Icons.jsx
@@ -1,3 +1,36 @@
 export default function Icons() {
-  return <section>Icons</section>;
+  return (
+    <section className="icons">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img
+              src="/imgs/buy-comics-digital-comics.png"
+              alt="Digital Comics"
+            />
+            <a href="#">Digital Comics</a>
+          </div>
+          <div className="col">
+            <img src="/imgs/buy-comics-merchandise.png" alt="DC Merchandise" />
+            <a href="#">DC Merchandise</a>
+          </div>
+          <div className="col">
+            <img src="/imgs/buy-comics-subscriptions.png" alt="Subscription" />
+            <a href="#">Subscription</a>
+          </div>
+          <div className="col">
+            <img
+              src="/imgs/buy-comics-shop-locator.png"
+              alt="Comic Store Locator"
+            />
+            <a href="#">Comic Store Locator</a>
+          </div>
+          <div className="col">
+            <img src="/imgs/buy-dc-power-visa.svg" alt="DC Power VISA" />
+            <a href="#">DC Power VISA</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
