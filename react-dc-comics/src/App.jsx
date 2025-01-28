4ceab0b7de830f77e099307bdf6b93b076b2d9dd
palascinochristian <palascinochristian@gmail.com>
2025-01-28 16:05:31 +0100

    init project

---
+++ react-dc-comics/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Icons from "./components/Icons";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Icons />
+      <Footer />
+    </>
+  );
+}
