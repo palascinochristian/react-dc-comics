d896b16601db650e2cb3bd57484852dc593e17ca
palascinochristian <palascinochristian@gmail.com>
2025-01-30 00:58:44 +0100

    feat: add  header links with react

---
+++ react-dc-comics/src/components/Data/headerLinks.js
@@ -0,0 +1,14 @@
+const links = [
+  "Characters",
+  "Comics",
+  "Movies",
+  "Tv",
+  "Games",
+  "Collectibles",
+  "Videos",
+  "Fans",
+  "News",
+  "Shop",
+];
+
+export default links;
--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,39 +1,16 @@
+import links from "../components/Data/headerLinks";
+
 export default function Header() {
   return (
     <header>
       <nav className="container">
         <img className="img-fluid" src="/imgs/dc-logo.png" />
         <ul>
-          <li>
-            <a href="#">Characters</a>
-          </li>
-          <li>
-            <a href="#">Comics</a>
-          </li>
-          <li>
-            <a href="#">Movies</a>
-          </li>
-          <li>
-            <a href="#">Tv</a>
-          </li>
-          <li>
-            <a href="#">Games</a>
-          </li>
-          <li>
-            <a href="#">Collectibles</a>
-          </li>
-          <li>
-            <a href="#">Videos</a>
-          </li>
-          <li>
-            <a href="#">Fans</a>
-          </li>
-          <li>
-            <a href="#">News</a>
-          </li>
-          <li>
-            <a href="#">Shop</a>
-          </li>
+          {links.map((link, index) => (
+            <li key={index}>
+              <a href="#">{link}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
